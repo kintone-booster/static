1690ea9f49cbd21d5778e9c637cb47cdbaab39d0
kintone-booster <141124813+kintone-booster@users.noreply.github.com>
2023-09-20 12:52:43 +0900

    Add files via upload

---
+++ plugins.mail.js
@@ -0,0 +1,10 @@
+/*
+* FileName "plugins.mail.js"
+* Version: 1.0.0
+* Copyright (c) 2023 Pandafirm LLC
+* Distributed under the terms of the GNU Lesser General Public License.
+* https://opensource.org/licenses/LGPL-2.1
+*/
+"use strict";
+((PLUGIN_ID) => {
+})(kintone.$PLUGIN_ID);
